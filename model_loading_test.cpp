2219022ccd5c7edc6327288181ee0072e4785bf3
oguzcannn <117283308+oguzcannn@users.noreply.github.com>
2026-05-15 14:06:02 +0300

    Tested button click and hover

--- model_loading_test.cpp
+++ model_loading_test.cpp
@@ -436,10 +436,17 @@ int main()
 
 		bool mouse_clicked = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
 
+		//mouse position in NDC for UI interactions
+		double mouse_xd, mouse_yd;
+		glfwGetCursorPos(window, &mouse_xd, &mouse_yd);
+
+		float mouse_ndc_x = (float)(mouse_xd / screen_width) * 2.0f - 1.0f;
+		float mouse_ndc_y = 1.0f - (float)(mouse_yd / screen_height) * 2.0f;
+
 		processInput(window, 0.1f * (float)((glfwGetTime() - time_of_last_frame) / Target_frame_time), fps_camera);
 		time_of_last_frame = glfwGetTime();
 
-		ui_manager.update(0.0f, 0.0f, mouse_clicked);
+		ui_manager.update(mouse_ndc_x, mouse_ndc_y, mouse_clicked);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
